b15ea320bdc324a9138f2c81588fc1007e0bfedd
Avinash <avinash.maurya@badabusiness.com>
2023-01-02 18:58:43 +0530

    add img

--- components/BigdataAnalytics/MainBanner.js
+++ components/BigdataAnalytics/MainBanner.js
@@ -7,8 +7,10 @@ const MainBanner = () => {
         <div className="bigdata-analytics-banner">
             <div className="container">
                 <div className="bigdata-analytics-content">
-                    <h1>Leading Big Data Analytics Compan2y</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida.</p>
+                    <h1>Cloud Computing & Hosting Services</h1>
+                    <p>Flexible Cloud Computing | Tailored-made Cloud for applications
+                        We offer you cloud hosting solutions with choices of OS, 
+                        applications and scalability over your public cloud.</p>
 
                     <Link href="/contact">
                         <a className="btn btn-primary">Get Started</a>
@@ -22,30 +24,30 @@ const MainBanner = () => {
                         <div className="col-lg-4 col-sm-6">
                             <div className="single-banner-boxes">
                                 <div className="icon">
-                                    <Icon.Server />
+                                     <img src="/images/cloud.png" />
                                 </div>
-                                <h3>Analyze Your Data</h3>
-                                <p>Lorem ipsum dolor sit amet elit, adipiscing, sed do eiusmod tempor incididunt ut labore dolore magna aliqua.</p>
+                                <h3>Fully Managed Cloud Hosting</h3>
+                                <p>We free you up to focus on your business!</p>
                             </div>
                         </div>
 
                         <div className="col-lg-4 col-sm-6">
                             <div className="single-banner-boxes">
                                 <div className="icon">
-                                    <Icon.Code />
+                                <img src="/images/backup.png" />
                                 </div>
-                                <h3>Customized Plan</h3>
-                                <p>Lorem ipsum dolor sit amet elit, adipiscing, sed do eiusmod tempor incididunt ut labore dolore magna aliqua.</p>
+                                <h3>Cloud Simplified</h3>
+                                <p>Secure, powerful and easily scalable</p>
                             </div>
                         </div>
 
                         <div className="col-lg-4 col-sm-6">
                             <div className="single-banner-boxes">
                                 <div className="icon">
-                                    <Icon.Users />
+                                <img src="/images/enterprise.png" />
                                 </div>
                                 <h3>Implement Solution</h3>
-                                <p>Lorem ipsum dolor sit amet elit, adipiscing, sed do eiusmod tempor incididunt ut labore dolore magna aliqua.</p>
+                                <p>Fast, reliable and efficient storage</p>
                             </div>
                         </div>
                     </div>
