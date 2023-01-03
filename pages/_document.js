c0039c13c8ed33a2e2c3c00cc15da4fa7784cd8d
Avinash <avinash.maurya@badabusiness.com>
2023-01-03 14:06:27 +0530

    add remvoe

--- pages/_document.js
+++ pages/_document.js
@@ -13,7 +13,7 @@ class MyDocument extends Document {
         return (
             <Html dir={dir} lang={locale}>
                 <Head>
-                    <link rel="icon" type="image/png" href="/images/favicon32.ico"></link>
+                    <link rel="icon" type="image/png" href="/images/favicon32.png"></link>
                 </Head>
                 <body>
                     <Main />
